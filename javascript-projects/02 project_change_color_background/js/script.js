c6e1d2a30e32d9e3a0bf7a83a2ee2359dc61e4a2
Aaron <kilpatrick.aaron92@gmail.com>
2021-02-23 20:48:00 +1100

    finished project 02

--- javascript-projects/02 project_change_color_background/js/script.js
+++ javascript-projects/02 project_change_color_background/js/script.js
@@ -1,12 +1,9 @@
-//Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
+// Colour array
+var colours = ["#A93226", "#B03A2E", "#76448A", "#2471A3", "#17A589", "#F1C40F", "#2E4053", "#E67E22"];
+var buttonSelector = document.querySelector("button");
 
-function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
-}
+buttonSelector.addEventListener("click", function() {
+  var randomColour = colours[Math.floor(Math.random() * 8)];
+  document.querySelector("body").style.backgroundColor = randomColour;
+})
